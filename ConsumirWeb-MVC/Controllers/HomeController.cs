2e66661ba161a111f40bee4b7b26ff095db37a6a
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2022-08-05 11:52:50 -0300

    first commit

---
+++ ConsumirWeb-MVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using ConsumirWeb_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ConsumirWeb_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            //https://jsonplaceholder.typicode.com/posts/");
+            var httpClient = new HttpClient();
+            var json_respuesta = await httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/posts/");
+            var result = JsonConvert.DeserializeObject<List<ObjetoObtenido>>(json_respuesta);
+            return View(result);
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ConsumirWeb-MVC/Models/ObjetoObtenido.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsumirWeb_MVC.Models
+{
+    public class ObjetoObtenido
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+}
